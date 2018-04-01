701e24e52310f85fcb7e81bdf74d8a9ed0d567e6
dokelung <dokelung@gmail.com>
2018-04-01 21:08:32 +0800

    add IntField and BoolField

--- tkouter/fields.py
+++ tkouter/fields.py
@@ -33,3 +33,49 @@ class StringField:
         if len(self.var.get()) >= self._max_length:
             return
         self.var.set(value)
+
+
+class BoolField:
+    """ basic field which is implemented by BooleanVar
+    """
+
+    def __init__(self, *, default=False):
+        self._var = None
+        self._default = default
+
+    @property
+    def var(self):
+        if self._var is None:
+            self._var = BooleanVar()
+            self._var.set(self._default)
+        else:
+            return self._var
+
+    def __get__(self, instance, owner):
+        return self.var.get()
+
+    def __set__(self, instance, value):
+        self.var.set(value)
+
+
+class IntField:
+    """ basic field which is implemented by IntVar
+    """
+
+    def __init__(self, *, default=0):
+        self._var = None
+        self._default = default
+
+    @property
+    def var(self):
+        if self._var is None:
+            self._var = IntVar()
+            self._var.set(self._default)
+        else:
+            return self._var
+
+    def __get__(self, instance, owner):
+        return self.var.get()
+
+    def __set__(self, instance, value):
+        self.var.set(value)
